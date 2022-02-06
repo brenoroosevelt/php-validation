5bbaed4b5208c78e3a458d9b700a0bc2690c7e8e
Breno Roosevelt <breno.jesus@ufms.br>
2022-02-06 17:10:04 -0400

    refactoring

--- src/Rules/AlwaysOk.php
+++ src/Rules/AlwaysOk.php
@@ -3,12 +3,10 @@ declare(strict_types=1);
 
 namespace BrenoRoosevelt\Validation\Rules;
 
-use Attribute;
 use BrenoRoosevelt\Validation\Result;
 use BrenoRoosevelt\Validation\Validation;
 use BrenoRoosevelt\Validation\ValidationResult;
 
-#[Attribute(Attribute::TARGET_PROPERTY)]
 class AlwaysOk implements Validation
 {
     public function validate($input, array $context = []): Result
