595cf41bac753a86cdc84bbeb6e808f15f4bd4a8
Breno Roosevelt <breno.jesus@ufms.br>
2022-02-07 17:24:56 -0400

    type rules

---
+++ src/Rules/Type/IsInstanceOfAny.php
@@ -0,0 +1,27 @@
+<?php
+declare(strict_types=1);
+
+namespace BrenoRoosevelt\Validation\Rules\Type;
+
+use Attribute;
+use BrenoRoosevelt\Validation\AbstractValidation;
+
+#[Attribute(Attribute::TARGET_PROPERTY)]
+class IsInstanceOfAny extends AbstractValidation
+{
+    public function __construct(private array $classes, ?string $message = null)
+    {
+        parent::__construct($message);
+    }
+
+    protected function evaluate($input, array $context = []): bool
+    {
+        foreach ($this->classes as $class) {
+            if ($input instanceof $class) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
