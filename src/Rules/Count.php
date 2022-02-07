ff172dbd7114583cd172c244efea809ca0497ec7
Breno Roosevelt <breno.jesus@ufms.br>
2022-02-07 17:11:58 -0400

    basic rules

---
+++ src/Rules/Count.php
@@ -0,0 +1,41 @@
+<?php
+declare(strict_types=1);
+
+namespace BrenoRoosevelt\Validation\Rules;
+
+use Attribute;
+use BrenoRoosevelt\Validation\AbstractValidation;
+use Traversable;
+
+#[Attribute(Attribute::TARGET_PROPERTY)]
+class Count extends AbstractValidation
+{
+    const MESSAGE = 'Expected count is: %s';
+
+    public function __construct(private int $count, string $message = null)
+    {
+        parent::__construct($message ?? sprintf(self::MESSAGE, $this->count));
+    }
+
+    protected function evaluate($input, array $context = []): bool
+    {
+        if (is_countable($input)) {
+            return count($input) === $this->count;
+        }
+
+        if ($input instanceof Traversable) {
+            return iterator_count($input) === $this->count;
+        }
+
+        if (is_iterable($input)) {
+            $count = 0;
+            foreach ($input as $v) {
+                $count++;
+            }
+
+            return $count === $this->count;
+        }
+
+        return false;
+    }
+}
---
+++ src/Rules/IsNull.php
@@ -0,0 +1,16 @@
+<?php
+declare(strict_types=1);
+
+namespace BrenoRoosevelt\Validation\Rules;
+
+use Attribute;
+use BrenoRoosevelt\Validation\AbstractValidation;
+
+#[Attribute(Attribute::TARGET_PROPERTY|Attribute::TARGET_METHOD)]
+class IsNull extends AbstractValidation
+{
+    protected function evaluate($input, array $context = []): bool
+    {
+        return $input === null;
+    }
+}
