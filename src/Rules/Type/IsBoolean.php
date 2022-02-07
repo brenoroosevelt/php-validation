8dd5f81b434a8eba288bbd979c70a1899a06f94d
Breno Roosevelt <breno.jesus@ufms.br>
2022-02-07 17:22:50 -0400

    type rules

---
+++ src/Rules/Type/IsBoolean.php
@@ -0,0 +1,16 @@
+<?php
+declare(strict_types=1);
+
+namespace BrenoRoosevelt\Validation\Rules\Type;
+
+use Attribute;
+use BrenoRoosevelt\Validation\AbstractValidation;
+
+#[Attribute(Attribute::TARGET_PROPERTY)]
+class IsBoolean extends AbstractValidation
+{
+    protected function evaluate($input, array $context = []): bool
+    {
+        return is_bool($input);
+    }
+}
