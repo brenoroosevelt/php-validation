0a0e8d09102975fc1e1139c5ca54cd28c0ac63dd
Breno Roosevelt <breno.jesus@ufms.br>
2022-02-07 12:41:50 -0400

    comparison validation

---
+++ src/Rules/Comparison/Exacly.php
@@ -0,0 +1,24 @@
+<?php
+declare(strict_types=1);
+
+namespace BrenoRoosevelt\Validation\Rules\Comparison;
+
+use Attribute;
+use BrenoRoosevelt\Validation\AbstractValidation;
+
+#[Attribute(Attribute::TARGET_PROPERTY)]
+class Exacly extends AbstractValidation
+{
+    const MESSAGE = 'The value should be exacly `%s`';
+
+    public function __construct(private mixed $value, ?string $message = null)
+    {
+        $this->message = $message ?? sprintf(self::MESSAGE, $this->value);
+        parent::__construct($message);
+    }
+
+    protected function evaluate($input, array $context = []): bool
+    {
+        return $input === $this->value;
+    }
+}
---
+++ src/Rules/Comparison/NumberBetween.php
@@ -0,0 +1,37 @@
+<?php
+declare(strict_types=1);
+
+namespace BrenoRoosevelt\Validation\Rules\Comparison;
+
+use Attribute;
+use BrenoRoosevelt\Validation\AbstractValidation;
+
+#[Attribute(Attribute::TARGET_PROPERTY)]
+class NumberBetween extends AbstractValidation
+{
+    const MESSAGE = 'the value should be between `%s` and `%s` (%s inclusive)';
+
+    public function __construct(
+        private int|float $min,
+        private int|float $max,
+        private $boundaries = true,
+        ?string $message = null
+    ) {
+        $this->message = $message ?? sprintf(
+            self::MESSAGE,
+            $this->min,
+                $this->max,
+                !$this->boundaries ? 'not' : ''
+            );
+        parent::__construct($message);
+    }
+
+    protected function evaluate($input, array $context = []): bool
+    {
+        if ($this->boundaries) {
+            return $input >= $this->min && $input <= $this->max;
+        }
+
+        return $input > $this->min && $input < $this->max;
+    }
+}
