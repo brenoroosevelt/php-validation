c581ab82ece5a98ddd373b75bb38ca608196b378
Breno Roosevelt <breno.jesus@ufms.br>
2022-02-07 12:47:43 -0400

    comparison validation

---
+++ src/Rules/Comparison/EqualOther.php
@@ -0,0 +1,24 @@
+<?php
+declare(strict_types=1);
+
+namespace BrenoRoosevelt\Validation\Rules\Comparison;
+
+use Attribute;
+use BrenoRoosevelt\Validation\AbstractValidation;
+
+#[Attribute(Attribute::TARGET_PROPERTY)]
+class EqualOther extends AbstractValidation
+{
+    const MESSAGE = 'The value should be equals to `%s`';
+
+    public function __construct(private mixed $other, ?string $message = null)
+    {
+        $this->message = $message ?? sprintf(self::MESSAGE, $this->other);
+        parent::__construct($message);
+    }
+
+    protected function evaluate($input, array $context = []): bool
+    {
+        return array_key_exists($this->other, $context) && $input == $context[$this->other];
+    }
+}
