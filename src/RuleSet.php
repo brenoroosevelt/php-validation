d2e529bece357fe8d6ac3bec5573e2cdcd3d7960
Breno Roosevelt <breno.jesus@ufms.br>
2022-02-08 14:40:50 -0400

    refacotring

--- src/RuleSet.php
+++ src/RuleSet.php
@@ -88,11 +88,16 @@ class RuleSet implements Rule, IteratorAggregate, Countable
             return false;
         }
 
-        if (is_string($input) && empty($input) && $this->allowsEmpty()) {
+        if ($this->inputTypeAllowsEmpty($input) && empty($input) && $this->allowsEmpty()) {
             return false;
         }
 
         return true;
+    }
+
+    private function inputTypeAllowsEmpty($input) : bool
+    {
+        return is_array($input) || is_string($input);
     }
 
     public function isRequired(): bool
