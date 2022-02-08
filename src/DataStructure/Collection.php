e8f85d1feefdc439193cbc60d1bdb1d06b14135d
Breno Roosevelt <breno.jesus@ufms.br>
2022-02-08 09:38:58 -0400

    refactoring

---
+++ src/DataStructure/Collection.php
@@ -0,0 +1,28 @@
+<?php
+declare(strict_types=1);
+
+namespace BrenoRoosevelt\Validation\DataStructure;
+
+use ArrayAccess;
+use Countable;
+use IteratorAggregate;
+
+class Collection implements IteratorAggregate, ArrayAccess, Countable
+{
+    use CollectionTrait {
+        insert as public;
+        deleteElement as public;
+        deleteByIndex as public;
+    }
+
+    use IteratorTrait,
+        CountableTrait,
+        ArrayAccessTrait;
+
+    public function __construct(iterable $values = [])
+    {
+        foreach ($values as $key => $value) {
+            $this->insert($value, $key);
+        }
+    }
+}
