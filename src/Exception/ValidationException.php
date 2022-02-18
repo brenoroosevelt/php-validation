1d09ace5b99207589d241d0ff7bea420aea4bb7d
Breno Roosevelt <breno.jesus@ufms.br>
2022-02-18 11:30:08 -0400

    api problem

--- src/Exception/ValidationException.php
+++ src/Exception/ValidationException.php
@@ -5,16 +5,18 @@ namespace BrenoRoosevelt\Validation\Exception;
 
 use BrenoRoosevelt\Validation\Error;
 use Exception;
+use JsonSerializable;
+use Phpro\ApiProblem\Http\HttpApiProblem;
 use Throwable;
 
-class ValidationException extends Exception implements ValidationExceptionInterface
+class ValidationException extends Exception implements ValidationExceptionInterface, JsonSerializable
 {
     private static ?ValidationExceptionFactoryInterface $userDefinedFactory = null;
 
     /** @var Error[] */
     private array $errors;
 
-    public function __construct(array $errors, ?string $message = "", int $code = 422, ?Throwable $previous = null)
+    public function __construct(array $errors, ?string $message = "", int $code = 400, ?Throwable $previous = null)
     {
         $this->errors = array_filter($errors, fn($e) => $e instanceof Error);
         parent::__construct($message ?? '', $code, $previous);
@@ -39,5 +41,45 @@ class ValidationException extends Exception implements ValidationExceptionInterf
     public static function getFactory(): ?ValidationExceptionFactoryInterface
     {
         return self::$userDefinedFactory;
+    }
+
+    /**
+     * Convert to HttpApiProblem
+     *
+     * @return HttpApiProblem
+     */
+    public function toApiProblem(): HttpApiProblem
+    {
+        return new HttpApiProblem($this->getCode(), [
+            'type' => HttpApiProblem::TYPE_HTTP_RFC,
+            'title' => HttpApiProblem::getTitleForStatusCode($this->getCode()),
+            'detail' => $this->message ?? 'Input validation failed',
+            'violations' => $this->serializeViolations(),
+        ]);
+    }
+
+    private function serializeViolations(): array
+    {
+        $violations = [];
+        foreach ($this->errors as $violation) {
+            $violationEntry = [
+                'propertyPath' => $violation->field(),
+                'title' => $violation->message(),
+            ];
+
+            $violations[] = $violationEntry;
+        }
+
+        return $violations;
+    }
+
+    /**
+     * Provides an RFC 7807 compliant response
+     *
+     * @return array
+     */
+    public function jsonSerialize(): array
+    {
+        return $this->toApiProblem()->toArray();
     }
 }
