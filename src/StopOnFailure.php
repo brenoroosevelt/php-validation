86dad527a3316563c79d94e05e38cc44e4eff325
Breno Roosevelt <breno.jesus@ufms.br>
2022-02-20 07:27:58 -0400

    fix invalid stop sign

--- src/StopOnFailure.php
+++ src/StopOnFailure.php
@@ -3,12 +3,19 @@ declare(strict_types=1);
 
 namespace BrenoRoosevelt\Validation;
 
+use BrenoRoosevelt\Validation\Rules\Choice;
+use InvalidArgumentException;
+
 trait StopOnFailure
 {
     protected int $stopOnFailure = StopSign::DONT_STOP;
 
     public function setStopOnFailure(int $stopSign): static
     {
+        if (! in_array($stopSign, StopSign::allowed())) {
+            throw new InvalidArgumentException(sprintf('Invalid stop sign (%s)', $stopSign));
+        }
+
         $instance = clone $this;
         $instance->stopOnFailure = $stopSign;
         return $instance;
--- src/StopSign.php
+++ src/StopSign.php
@@ -3,15 +3,18 @@ declare(strict_types=1);
 
 namespace BrenoRoosevelt\Validation;
 
-/**
- * @codeCoverageIgnore
- */
 final class StopSign
 {
     const DONT_STOP = 0;
     const SAME_FIELD = 1;
     const ALL = 2;
 
+    public static function allowed(): array
+    {
+        return [self::DONT_STOP, self::SAME_FIELD, self::ALL];
+    }
+
+    /** @codeCoverageIgnore */
     private function __construct()
     {
     }
