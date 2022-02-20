00f1823acb6a15f84ae34af60c40c42c2a82fc00
Breno Roosevelt <breno.jesus@ufms.br>
2022-02-20 10:53:54 -0400

    comparison

--- src/Comparator.php
+++ src/Comparator.php
@@ -11,16 +11,16 @@ trait Comparator
     public function compare(mixed $a, string $operator, mixed $b): bool
     {
         return
-        match ($operator) {
-            Operator::EQUAL => $a == $b,
-            Operator::EXACTLY => $a === $b,
-            Operator::GREATER_THAN => $a > $b,
-            Operator::GREATER_THAN_EQUAL => $a >= $b,
-            Operator::LESS_THAN => $a < $b,
-            Operator::LESS_THAN_EQUAL => $a <= $b,
-            Operator::NOT_EQUAL => $a != $b,
-            Operator::NOT_EXACTLY => $a !== $b,
-            default => throw new InvalidArgumentException(sprintf('Invalid operator: %s', $operator))
-        };
+            match ($operator) {
+                Operator::EQUAL => $a == $b,
+                Operator::EXACTLY => $a === $b,
+                Operator::GREATER_THAN => $a > $b,
+                Operator::GREATER_THAN_EQUAL => $a >= $b,
+                Operator::LESS_THAN => $a < $b,
+                Operator::LESS_THAN_EQUAL => $a <= $b,
+                Operator::NOT_EQUAL => $a != $b,
+                Operator::NOT_EXACTLY => $a !== $b,
+                default => throw new InvalidArgumentException(sprintf('Invalid operator: %s', $operator))
+            };
     }
 }
