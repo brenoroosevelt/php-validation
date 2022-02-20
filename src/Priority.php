11848cf58220fcb07e064a154df23c6a3ed13fe2
Breno Roosevelt <breno.jesus@ufms.br>
2022-02-20 09:43:53 -0400

    refactoring

--- src/Priority.php
+++ src/Priority.php
@@ -29,8 +29,8 @@ trait Priority
     public static function sortByPriority(array &$haystack): void
     {
         uasort($haystack, function ($a, $b) {
-            $prorityOfA = $a instanceof Prioritable ? $a->getPriority() : 0;
-            $prorityOfB = $b instanceof Prioritable ? $b->getPriority() : 0;
+            $prorityOfA = $a instanceof Prioritable ? $a->getPriority() : Prioritable::LOWEST_PRIORITY;
+            $prorityOfB = $b instanceof Prioritable ? $b->getPriority() : Prioritable::LOWEST_PRIORITY;
 
             return $prorityOfA <=> $prorityOfB;
         });
